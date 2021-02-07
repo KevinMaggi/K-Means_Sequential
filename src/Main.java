2a2fa6bc2d9d6337e662e3269f74daa9d6c7b9eb
kevin <kevin.maggi@stud.unifi.it>
2021-02-07 18:02:23 +0100

    Main improvements and test images

--- src/Main.java
+++ src/Main.java
@@ -6,53 +6,75 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-    private static final int K_MIN = 10;
-    private static final int K_MAX = 10;
+    /**
+     * Min value of K to test
+     */
+    private static final int K_MIN = 6;
+    /**
+     * Max value of K to test
+     */
+    private static final int K_MAX = 8;
+    /**
+     * Step on values of K
+     */
     private static final int K_STEP = 2;
-    private static final int REPETITIONS = 3;
+
+    /**
+     * Image dimension to test: 4K, 5K or 6K
+     */
+    private static final String IMAGE_DIMENSION = "4K";
+    /**
+     * Number of image of each dimension to test (max 3)
+     */
+    private static final int IMAGE_QUANTITY = 3;
+
+    /**
+     * Number of times to test each image
+     */
+    private static final int REPETITIONS = 2;
 
     public static void main(String[] args) {
         try {
             int[] keys = new int[K_MAX - K_MIN + 1];
-            double[] times = new double[K_MAX - K_MIN + 1];
-            String filename = "6K";
-
-            BufferedImage img = Image.load("src/image/" + filename + ".jpg");
-            SetOfPoints<RGBPixel> data = Image.pixelize(img);
+            float[] times = new float[K_MAX - K_MIN + 1];
 
             KMeans<RGBPixel> kmeans = new KMeans<>();
 
             for (int k = K_MIN; k <= K_MAX; k += K_STEP) {
-                ArrayList<Cluster<RGBPixel>> clusters = null;
-                double diffTimeMillis = 0;
-                for (int i = 0; i < REPETITIONS; i++) {
-                    clusters = null;        // to garbage collect previous result and avoid Heap Space Error
+                long cumulativeTimesMillis = 0;
+                for (int imageIndex = 1; imageIndex <= IMAGE_QUANTITY; imageIndex++) {
+                    BufferedImage img = Image.load("src/image/" + IMAGE_DIMENSION + "-" + imageIndex + ".jpg");
+                    SetOfPoints<RGBPixel> data = Image.pixelize(img);
 
-                    long startTimeMillis = System.currentTimeMillis();
-                    clusters = kmeans.clusterize(k, data);
-                    long endTimeMillis = System.currentTimeMillis();
+                    ArrayList<Cluster<RGBPixel>> clusters = null;
+                    for (int i = 0; i < REPETITIONS; i++) {
+                        clusters = null;        // to garbage collect previous result and avoid Heap Space Error
 
-                    diffTimeMillis += endTimeMillis - startTimeMillis;
-                    System.out.println("For k = " + Integer.toString(k) + ", computation time is " + Double.toString((endTimeMillis-startTimeMillis)/1000d) + " seconds");
+                        long startTimeMillis = System.currentTimeMillis();
+                        clusters = kmeans.clusterize(k, data);
+                        long endTimeMillis = System.currentTimeMillis();
+
+                        cumulativeTimesMillis += endTimeMillis - startTimeMillis;
+                        System.out.println("For k = " + k + " on image" + imageIndex + ", computation time is " + (endTimeMillis-startTimeMillis)/1000f + " seconds");
+                    }
+
+                    Image.export(clusters, "out/results/" + IMAGE_DIMENSION + "-" + imageIndex + "-quantized" + k + ".png", img.getWidth(), img.getHeight());
                 }
-
-                Image.export(clusters, "src/image/" + filename + "-reduced" + k + ".png", img.getWidth(), img.getHeight());
-
-                double meanTimeSeconds = diffTimeMillis / (1000d * REPETITIONS);
+                float meanTimeSeconds = cumulativeTimesMillis / (1000f * REPETITIONS * IMAGE_QUANTITY);
                 keys[k - K_MIN] = k;
                 times[k - K_MIN] = meanTimeSeconds;
-                System.out.println("For k = " + Integer.toString(k) + ", MEAN computation time is " + Double.toString(meanTimeSeconds) + " seconds");
+                System.out.println("For k = " + k + ", MEAN computation time is " + meanTimeSeconds + " seconds");
             }
 
-            saveTextFile(keys, times, "timesOf" + filename);
+            saveTextFile(keys, times, "timesOf" + IMAGE_DIMENSION);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void saveTextFile(final int[] kList, final double[] timeList, String fileName) {
-        String path = "src/image/" + fileName + ".txt";
+    private static void saveTextFile(final int[] kList, final float[] timeList, String fileName) {
+        String path = "out/results/" + fileName + ".txt";
         try {
             File file = new File(path);
             BufferedWriter output = new BufferedWriter(new FileWriter(file));
